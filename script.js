cb3e3fba8987e7792ffeb3c6ac63972b7816aee0
AaronM524 <aaronmathew468@gmail.com>
2024-02-01 23:04:33 -0500

    finished

--- script.js
+++ script.js
@@ -1,33 +1,35 @@
-function Change2006() {
-    document.getElementById('Change-Image').src = 'images/me.jpg' 
+function Change2007() {
+    document.getElementById('Change-Image').src = 'images/IMG_9901.JPG'
+    document.getElementById('Change-Text').innerText = 'This was me in Florida while on vacation for spring break.'
+
 }
 
-function Change1944() {
-document.getElementById('Change-Image').src = 'images/paris.jpg'
-document.getElementById('Change-Text').innerText = ' Paris is liberated after four years of Nazi occupation'
-document.getElementById('Change-Date').innerText = 'August 25, 1944'
+function Change1264() {
+    document.getElementById('Change-Image').src = 'images/YuanEmperorAlbumTemurOljeituPortrait_500x500.jpg'
+    document.getElementById('Change-Text').innerText = 'The Khagan Emperor, Kublai Khan of the Mongol Empire, surrendered against his brother. This ended the Toluid Civil War.'
+    document.getElementById('Change-Date').innerText = 'August 21, 1264'
+    }
+
+function Change1858() {
+document.getElementById('Change-Image').src = 'images/Stephen-A-Douglas-Abraham-Lincoln-debate-1858_500x500.jpg'
+document.getElementById('Change-Text').innerText = 'The first of 7 debates had occured between the famous Abraham Lincoln and Stephen Douglas.'
+document.getElementById('Change-Date').innerText = 'August 21, 1858'
 }
 
-function Change1979() {
-document.getElementById('Change-Image').src = 'images/david.jpg'
-document.getElementById('Change-Text').innerText = 'On August 25, 1979, the storm that will become Hurricane David forms near Cape Verde off the African coast in the eastern Atlantic Ocean. It would go on to devastate the island of Dominica, and then the Dominican Republic, killing 1,500 people.'
-document.getElementById('Change-Date').innerText = 'August 25, 1979'
+function Change1911() {
+document.getElementById('Change-Image').src = 'images/Monalisa_uffizi_in1913_500x500.jpg'
+document.getElementById('Change-Text').innerText = 'Leonardo da Vinci‘s well-known painting, the Mona Lisa, was stolen from Louvre, Paris. The painting was stolen by an Italian man named Vincenzo Pergguia.'
+document.getElementById('Change-Date').innerText = 'August 21, 1911'
 }
 
-function Change1984() {
-document.getElementById('Change-Image').src = 'images/capote.jpg'
-document.getElementById('Change-Text').innerText = 'Truman Capote, the author of the pioneering true-crime novel In Cold Blood, dies at age 59 in Los Angeles.'
-document.getElementById('Change-Date').innerText = 'August 25, 1984'
+function Change1959() {
+document.getElementById('Change-Image').src = 'images/hawaii_plane.jpg'
+document.getElementById('Change-Text').innerText = 'Hawaii becomes the 50th state, making it the last to join the Union.'
+document.getElementById('Change-Date').innerText = 'August 21, 1959'
 }
 
-function Change1985() {
-document.getElementById('Change-Image').src = 'images/smith.jpg'
-document.getElementById('Change-Text').innerText = 'Samantha Smith, the 13-year-old “ambassador” to the Soviet Union, dies in a plane crash. Smith was best known for writing to Soviet leader Yuri Andropov in 1982 and visiting the Soviet Union as Andropov’s guest in 1983.'
-document.getElementById('Change-Date').innerText = 'August 25, 1985'
-}
-
-function Change2009() {
-document.getElementById('Change-Image').src = 'images/ted.jpg'
-document.getElementById('Change-Text').innerText = 'On August 25, 2009, Edward “Ted” Kennedy, the youngest brother of President John F. Kennedy and a U.S. senator from Massachusetts from 1962 to 2009, dies of brain cancer at age 77 at his home in Hyannis Port, Massachusetts. Kennedy, one of the longest-serving senators in American history, was a leader of the Democratic Party and a spokesman for liberal causes who also was known for his ability to work with those on both sides of the political aisle.'
-document.getElementById('Change-Date').innerText = 'August 25, 2009'
+function Change1996() {
+document.getElementById('Change-Image').src = 'images/tiger.jpg'
+document.getElementById('Change-Text').innerText = 'Tiger Woods made history by winning the 82nd PGA Championship, which marked it his second consecutive win.'
+document.getElementById('Change-Date').innerText = 'August 21, 1996'
 }
